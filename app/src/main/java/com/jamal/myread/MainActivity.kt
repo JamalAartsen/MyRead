b5ad570481c735b34a3380a4250ef57f7b0d5274
J.A. Aartsen <jamal.aartsen@hva.nl>
2021-12-27 23:34:50 +0100

    Add splash screen

--- app/src/main/java/com/jamal/myread/MainActivity.kt
+++ app/src/main/java/com/jamal/myread/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_MyRead)
         setContentView(binding.root)
     }
 }
