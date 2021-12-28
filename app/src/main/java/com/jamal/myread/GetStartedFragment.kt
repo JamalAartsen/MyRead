35c8cc82d4ba23b77494d0e0da9447ab4224c4c2
J.A. Aartsen <jamal.aartsen@hva.nl>
2021-12-28 12:03:28 +0100

    Add navigation component

---
+++ app/src/main/java/com/jamal/myread/GetStartedFragment.kt
@@ -0,0 +1,38 @@
+package com.jamal.myread
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.jamal.myread.databinding.FragmentGetStartedBinding
+
+class GetStartedFragment : Fragment(R.layout.fragment_get_started) {
+
+    private var _binding: FragmentGetStartedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGetStartedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.introGetStartedBtn.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.actionNavigateToHomeFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/jamal/myread/MainActivity.kt
+++ app/src/main/java/com/jamal/myread/MainActivity.kt
@@ -2,13 +2,37 @@ package com.jamal.myread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.jamal.myread.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_MyRead)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == R.id.getStartedFragment) {
+                Log.d(TAG, "onCreate: Good")
+                binding.toolbar.visibility = View.GONE
+            } else {
+                Log.d(TAG, "onCreate: ${destination.id} ${R.id.getStartedFragment}")
+                binding.toolbar.visibility = View.VISIBLE
+            }
+
+        }
     }
 }
